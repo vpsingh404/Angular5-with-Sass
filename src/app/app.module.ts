a192b8c6db8917b47d563b9cd9c7184e1ac36ffa
vijay pratap singh <vijay.singh2@globallogic.com>
2018-09-12 13:42:42 +0530

    new files added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Ng2HandySyntaxHighlighterModule } from 'ng2-handy-syntax-highlighter';
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Ng2HandySyntaxHighlighterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
